5812b3c17106503492bb0caf98b1257bd8019ce8
Carl <carlhuang@sunnygo.com.tw>
2023-04-06 09:22:59 +0800

    增加配置

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,36 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-// import styleImport, { AntdResolve } from "vite-plugin-style-import";
 import path from "path";
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    react(),
-    // styleImport({
-    //   resolves: [AntdResolve()],
-    // }),
-  ],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
+export default defineConfig(({ command, mode }) => {
+  if (mode === "production") {
+    return {
+      plugins: [react()],
+      resolve: {
+        alias: {
+          "@": path.resolve(__dirname, "./src"),
+        },
+      },
+      build: {
+        outDir: "prodBuild",
+      },
+      base: "./",
+    };
+  } else {
+    return {
+      plugins: [react()],
+      resolve: {
+        alias: {
+          "@": path.resolve(__dirname, "./src"),
+        },
+      },
+      server: {
+        proxy: {
+          "/api": {
+            target: "http://localhost:9000",
+            changeOrigin: true,
+          },
+        },
+      },
+    };
+  }
 });
